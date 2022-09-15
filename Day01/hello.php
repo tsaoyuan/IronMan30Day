7df237b30b1043cef1500307f162483c808625d7
tsao <tsaoyuan@gmail.com>
2022-09-15 22:49:18 +0800

    Hello PHP

---
+++ Day01/hello.php
@@ -0,0 +1,11 @@
+<?php
+  echo "Hello PHP!🤟</br></br>";
+  echo "Hello PHP!🤟 </br></br>";
+  echo "Hello PHP!🤟";
+  echo "<h2>Hello</h2>😊PHP!🤟";
+
+  echo "<h2>Hello</h2>😊PHP!🤟";
+  echo "<h2>Hello</h2>😊PHP!🤟";
+  echo "<h2>✅✅Hello</h2>😊PHP!🤟";
+
+  ?>
